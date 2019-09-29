1febee2ae39e939da01c68a9fef09564cdc559a3
Alec Cantor <apc47@pitt.edu>
2019-09-29 13:06:55 -0700

    Create init file system

---
+++ build/CMakeFiles/Tanks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tanks.dir/GameStartUp.cpp.o"
+  "Tanks.pdb"
+  "Tanks"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tanks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
