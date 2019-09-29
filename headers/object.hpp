6f7b61cd428e73df69ace2d485eb4bca3fd2b82e
Jakob-OSX <jakobpstrobl@gmail.com>
2019-09-29 02:23:16 -0400

    Added abstract object header file; object.hpp

---
+++ headers/object.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file object.hpp
+ * @author Networking Team (Adam Alec Jakob)
+ * @brief Header file for abstract class object (implemented by general game entities) in object.cpp
+ * @version 0.1
+ * @date 2019-09-29
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+class OBJECT;
