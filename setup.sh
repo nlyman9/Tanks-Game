cd8ef59643016e4d058a0f5405ba8301fb32be7a
Alec Cantor <apc47@pitt.edu>
2019-09-30 10:17:42 -0700

    Add libmad to repo

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ sudo apt-get install libsdl2-mixer-dev
 sudo apt-get install libsdl2-ttf-dev
 sudo apt-get install libsdl2-image-dev
 sudo apt-get install libsndio-dev
+sudo apt-get install libmad0-dev
+sudo apt-get install libmadlib-dev
