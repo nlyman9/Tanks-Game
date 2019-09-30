4e70931bd272e02f0844ba344d642737e1ab4df1
Alec Cantor <apc47@pitt.edu>
2019-09-30 12:24:45 -0700

    Change libsndio to older version in install script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ sudo apt-get install libsdl2-dev -y
 sudo apt-get install libsdl2-mixer-dev -y
 sudo apt-get install libsdl2-ttf-dev -y
 sudo apt-get install libsdl2-image-dev -y
-sudo apt-get install libsndio-dev -y
+sudo apt-get install libsndio6.1 -y
 sudo apt-get install libmad0-dev -y
 sudo apt-get install libmadlib-dev -y
