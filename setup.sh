4b194131aa1c6daaa3810b0d19f123619fd94576
Alec Cantor <apc47@pitt.edu>
2019-09-30 11:04:21 -0700

    Add -y to setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-sudo apt-get install libsdl2-dev
-sudo apt-get install libsdl2-mixer-dev
-sudo apt-get install libsdl2-ttf-dev
-sudo apt-get install libsdl2-image-dev
-sudo apt-get install libsndio-dev
-sudo apt-get install libmad0-dev
-sudo apt-get install libmadlib-dev
+sudo apt-get install libsdl2-dev -y
+sudo apt-get install libsdl2-mixer-dev -y
+sudo apt-get install libsdl2-ttf-dev -y
+sudo apt-get install libsdl2-image-dev -y
+sudo apt-get install libsndio-dev -y
+sudo apt-get install libmad0-dev -y
+sudo apt-get install libmadlib-dev -y
