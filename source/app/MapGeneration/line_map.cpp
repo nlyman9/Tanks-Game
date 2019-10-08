b3d6f1cfe79b98b5d127f63edbb35cc44babeca2
Dylan Miao <dtm32@pitt.edu>
2019-10-08 18:35:09 -0400

    updated map gen files

---
+++ source/app/MapGeneration/line_map.cpp
@@ -0,0 +1,47 @@
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+constexpr int X_WIDE = 24;
+constexpr int Y_HIGH = 13;
+
+int room[X_WIDE][Y_HIGH];
+
+int** linesMap () {
+	// make empty room array
+	int** array = 0;
+    array = new int*[24];
+
+    for(int i = 0; i < 24; i++)
+    {
+        array[i] = new int[13];
+        for(int j = 0; j < 13; j++)
+        {
+            array[i][j] = 0;
+        }
+    }
+
+	int** room = array;
+
+	//initialize random seed
+	srand(time(NULL));
+	int random_index;
+	for(int i = 0; i < 13; i++) {
+		random_index = rand() % 24;
+
+		for(int j = 0; j < 24; j++) {
+			if(i % 2 != 0) {
+				if(j == random_index)
+					tile_map[j][i] = 0;
+				else
+					tile_map[j][i] = 2;
+			}
+			else
+				tile_map[j][i] = 0;
+		}
+	}
+	return room;
+}
