44f3c9c9f5962705b37e665836b954482fcf1d4f
aclewell3 <51464370+aclewell3@users.noreply.github.com>
2019-10-08 17:59:35 -0700

    Fixed tiling background

--- Tests/game.cpp
+++ Tests/game.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <time.h>
+#include <math.h>
 
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 720;
@@ -23,6 +27,8 @@ bool init();
 SDL_Texture* loadImage(std::string fname);
 bool check_vicinity(SDL_Rect* a, SDL_Rect* b);
 bool player_vicinity(SDL_Rect* a, SDL_Rect* b);
+int** generateLineMap();
+int** generateMirrorMap();
 void close();
 
 // Globals
@@ -38,6 +44,131 @@ int** tile_map;
 std::vector<SDL_Texture*> gTex;
 SDL_Rect cur_out;
 
+
+bool init() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		std::cout << "Warning: Linear texture filtering not enabled!" << std::endl;
+	}
+
+	gWindow = SDL_CreateWindow("TANKS!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (gWindow == nullptr) {
+		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return  false;
+	}
+
+	// Adding VSync to avoid absurd framerates
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (gRenderer == nullptr) {
+		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return  false;
+	}
+
+	int imgFlags = IMG_INIT_PNG;
+	int retFlags = IMG_Init(imgFlags);
+	if(retFlags != imgFlags) {
+		std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+		return false;
+	}
+
+	// Set renderer draw/clear color
+	SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+
+	gTileSheet = loadImage("source/res/images/tiles.png");
+	for (int i = 0; i < 3; i++) {
+			gTileRects[i].x = i * TILE_SIZE;
+			gTileRects[i].y = 0;
+			gTileRects[i].w = TILE_SIZE;
+			gTileRects[i].h = TILE_SIZE;
+	}
+
+	// Fill 2D tile array of tiles with all 0s
+	// int** array = 0;
+	tile_map = new int*[24];
+
+	for(int j = 0; j < 24; j++)
+	{
+		tile_map[j] = new int[13];
+		for(int h = 0; h < 13; h++)
+		{
+			tile_map[j][h] = 0;
+		}
+	}
+
+	//small randomly generated thing
+
+	// Select map generation technique
+	enum map_types { destructible, holes, line, maze, mirror };
+	srand(time(NULL));
+
+	// switch(rand() % 4)
+	switch(line)
+	{
+		case destructible:
+			tile_map[4][4] = 2;
+			break;
+		case holes:
+			tile_map[1][1] = 2;
+			break;
+		case line:
+			tile_map = generateLineMap();
+			break;
+		case maze:
+			tile_map[6][10] = 2;
+			break;
+		case mirror:
+			tile_map = generateMirrorMap();
+			tile_map[14][10] = 2;
+			break;
+	}
+
+
+
+	return true;
+}
+
+
+
+SDL_Texture* loadImage(std::string fname) {
+	SDL_Texture* newText = nullptr;
+
+	SDL_Surface* startSurf = IMG_Load(fname.c_str());
+	if (startSurf == nullptr) {
+		std::cout << "Unable to load image " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
+		return nullptr;
+	}
+
+	newText = SDL_CreateTextureFromSurface(gRenderer, startSurf);
+	if (newText == nullptr) {
+		std::cout << "Unable to create texture from " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_FreeSurface(startSurf);
+
+	return newText;
+}
+
+
+void close() {
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+	gRenderer = nullptr;
+
+	SDL_DestroyTexture(gTileSheet);
+	SDL_DestroyTexture(gTank_Red);
+	SDL_DestroyTexture(gTank_Blue);
+	gTileSheet = nullptr;
+	gTank_Red = nullptr;
+	gTank_Blue = nullptr;
+
+	// Quit SDL subsystems
+	SDL_Quit();
+}
 
 int** generateLineMap() {
 	// make empty room array
@@ -110,128 +241,6 @@ int** generateMirrorMap() //tile_map is 24x13
     return array;
 }
 
-bool init() {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-		return false;
-	}
-
-	if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
-		std::cout << "Warning: Linear texture filtering not enabled!" << std::endl;
-	}
-
-	gWindow = SDL_CreateWindow("Tiling", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (gWindow == nullptr) {
-		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-		return  false;
-	}
-
-	// Adding VSync to avoid absurd framerates
-	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (gRenderer == nullptr) {
-		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-		return  false;
-	}
-
-	int imgFlags = IMG_INIT_PNG;
-	int retFlags = IMG_Init(imgFlags);
-	if(retFlags != imgFlags) {
-		std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
-		return false;
-	}
-
-	// Set renderer draw/clear color
-	SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
-
-	gTileSheet = loadImage("source/res/images/tiles.png");
-	for (int i = 0; i < 3; i++) {
-			gTileRects[i].x = i * TILE_SIZE;
-			gTileRects[i].y = 0;
-			gTileRects[i].w = TILE_SIZE;
-			gTileRects[i].h = TILE_SIZE;
-	}
-
-	// Fill 2D tile array of tiles with all 0s
-	// int** array = 0;
-	tile_map = new int*[24];
-
-	for(int j = 0; j < 24; j++)
-	{
-		tile_map[j] = new int[13];
-		for(int h = 0; h < 13; h++)
-		{
-			tile_map[j][h] = 0;
-		}
-	}
-
-	//small randomly generated thing
-
-	// Select map generation technique
-	enum map_types { destructible, holes, line, maze, mirror };
-	srand(time(NULL));
-
-	// switch(rand() % 4)
-	switch(line)
-	{
-		case destructible:
-			tile_map[4][4] = 2;
-			break;
-		case holes:
-			tile_map[1][1] = 2;
-			break;
-		case line:
-			tile_map = generateLineMap();
-			break;
-		case maze:
-			tile_map[6][10] = 2;
-			break;
-		case mirror:
-			tile_map = generateMirrorMap();
-			tile_map[14][10] = 2;
-			break;
-	}
-
-	return true;
-}
-
-
-
-SDL_Texture* loadImage(std::string fname) {
-	SDL_Texture* newText = nullptr;
-
-	SDL_Surface* startSurf = IMG_Load(fname.c_str());
-	if (startSurf == nullptr) {
-		std::cout << "Unable to load image " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
-		return nullptr;
-	}
-
-	newText = SDL_CreateTextureFromSurface(gRenderer, startSurf);
-	if (newText == nullptr) {
-		std::cout << "Unable to create texture from " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
-	}
-
-	SDL_FreeSurface(startSurf);
-
-	return newText;
-}
-
-
-void close() {
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	gWindow = nullptr;
-	gRenderer = nullptr;
-
-	SDL_DestroyTexture(gTileSheet);
-	SDL_DestroyTexture(gTank_Red);
-	SDL_DestroyTexture(gTank_Blue);
-	gTileSheet = nullptr;
-	gTank_Red = nullptr;
-	gTank_Blue = nullptr;
-
-	// Quit SDL subsystems
-	SDL_Quit();
-}
 bool checkDistFrom(int playX, int playY, int enemX, int enemY) {
 
 	double  stepOne = (double) (pow((playX - enemX), 2)+pow((playY - enemY), 2));
@@ -610,6 +619,13 @@ int main() {
 			}
 		}
 
+		// Render array of tiles
+		for (int x = BORDER_GAP + TILE_SIZE, i = 0; x < SCREEN_WIDTH - BORDER_GAP - TILE_SIZE; x+=TILE_SIZE, i++) {
+			for (int y = TILE_SIZE, j = 0; y < SCREEN_HEIGHT - TILE_SIZE; y+=TILE_SIZE, j++) {
+				cur_out = { x, y, TILE_SIZE, TILE_SIZE};
+				SDL_RenderCopy(gRenderer, gTileSheet, &gTileRects[tile_map[i][j]], &cur_out);
+			}
+		}
 
 		//GENERATES TOP BORDER
 		c = BORDER_GAP;
@@ -642,7 +658,6 @@ int main() {
 		//Render player box
 		SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0xFF, 0xFF);
 		SDL_Rect fillRect = {x_pos, y_pos, BOX_WIDTH, BOX_HEIGHT};
-		//SDL_RenderFillRect(gRenderer, &fillRect);
 		SDL_RenderCopy(gRenderer, gTank_Blue, NULL, &fillRect);
 		//Render obstacle 1
 		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0xFF, 0xFF);
