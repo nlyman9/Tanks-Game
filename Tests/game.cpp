e342f399fc44da4147b068a375397c5dac2fd942
aclewell3 <51464370+aclewell3@users.noreply.github.com>
2019-10-08 17:35:01 -0700

    Overall game with random gen attempt

--- Tests/game.cpp
+++ Tests/game.cpp
@@ -34,6 +34,81 @@ SDL_Texture* gTank_Blue;
 SDL_Rect gTileRects[3];
 //true for testing bottom right to top left, false top left to bottom right
 bool dir = true;
+int** tile_map;
+std::vector<SDL_Texture*> gTex;
+SDL_Rect cur_out;
+
+
+int** generateLineMap() {
+	// make empty room array
+	int** array = 0;
+    array = new int*[24];
+
+    for(int i = 0; i < 24; i++)
+    {
+        array[i] = new int[13];
+        for(int j = 0; j < 13; j++)
+        {
+            array[i][j] = 0;
+        }
+    }
+
+	int** room = array;
+
+	//initialize random seed
+	srand(time(NULL));
+	int random_index;
+	int random_index_bonus;
+	for(int i = 0; i < 13; i++) {
+		random_index = rand() % 24;
+		if(rand() % 3 == 0)
+		{
+			random_index_bonus = rand() % 24;
+			if(abs(random_index_bonus - random_index) == 1)
+			{
+				random_index_bonus = -1;
+			}
+		}
+		else
+		{
+			random_index_bonus = -1;
+		}
+
+
+		for(int j = 0; j < 24; j++) {
+			if(i % 2 != 0) {
+				if(j == random_index || j == random_index_bonus)
+					room[j][i] = 0;
+				else
+					room[j][i] = 2;
+			}
+			else
+				room[j][i] = 0;
+		}
+	}
+	return room;
+}
+
+int** generateMirrorMap() //tile_map is 24x13
+{
+    int** array = 0;
+    array = new int*[24];
+
+    for(int i = 0; i < 24; i++)
+    {
+        array[i] = new int[13];
+        for(int j = 0; j < 13; j++)
+        {
+            array[i][j] = 0;
+        }
+    }
+
+    srand(time(NULL));
+
+    array[13][10] = 2;
+
+    return array;
+}
 
 bool init() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -68,8 +143,58 @@ bool init() {
 	// Set renderer draw/clear color
 	SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
 
+	gTileSheet = loadImage("source/res/images/tiles.png");
+	for (int i = 0; i < 3; i++) {
+			gTileRects[i].x = i * TILE_SIZE;
+			gTileRects[i].y = 0;
+			gTileRects[i].w = TILE_SIZE;
+			gTileRects[i].h = TILE_SIZE;
+	}
+
+	// Fill 2D tile array of tiles with all 0s
+	// int** array = 0;
+	tile_map = new int*[24];
+
+	for(int j = 0; j < 24; j++)
+	{
+		tile_map[j] = new int[13];
+		for(int h = 0; h < 13; h++)
+		{
+			tile_map[j][h] = 0;
+		}
+	}
+
+	//small randomly generated thing
+
+	// Select map generation technique
+	enum map_types { destructible, holes, line, maze, mirror };
+	srand(time(NULL));
+
+	// switch(rand() % 4)
+	switch(line)
+	{
+		case destructible:
+			tile_map[4][4] = 2;
+			break;
+		case holes:
+			tile_map[1][1] = 2;
+			break;
+		case line:
+			tile_map = generateLineMap();
+			break;
+		case maze:
+			tile_map[6][10] = 2;
+			break;
+		case mirror:
+			tile_map = generateMirrorMap();
+			tile_map[14][10] = 2;
+			break;
+	}
+
 	return true;
 }
+
+
 
 SDL_Texture* loadImage(std::string fname) {
 	SDL_Texture* newText = nullptr;
@@ -187,9 +312,9 @@ int main() {
 		return 1;
 	}
 
-	gTileSheet = loadImage("../source/res/images/tiles.png");
-	gTank_Red = loadImage("../source/res/images/red_tank.png");
-	gTank_Blue = loadImage("../source/res/images/blue_tank.png");
+	gTileSheet = loadImage("source/res/images/tiles.png");
+	gTank_Red = loadImage("source/res/images/red_tank.png");
+	gTank_Blue = loadImage("source/res/images/blue_tank.png");
 
 	for (int i = 0; i < 3; i++) {
 		gTileRects[i].x = i * TILE_SIZE;
@@ -468,6 +593,14 @@ int main() {
 		SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderClear(gRenderer);
 
+		// Render array of tiles
+		for (int x = BORDER_GAP + TILE_SIZE, i = 0; x < SCREEN_WIDTH - BORDER_GAP - TILE_SIZE; x+=TILE_SIZE, i++) {
+			for (int y = TILE_SIZE, j = 0; y < SCREEN_HEIGHT - TILE_SIZE; y+=TILE_SIZE, j++) {
+				cur_out = { x, y, TILE_SIZE, TILE_SIZE};
+				SDL_RenderCopy(gRenderer, gTileSheet, &gTileRects[tile_map[i][j]], &cur_out);
+			}
+		}
+
 		//FROM BORDERGAP + TILE SIZE TO GET INTERIOR OF MAP
 		//x represents the pixels of the screen, not the tile index anymore
 		for (int x = BORDER_GAP + TILE_SIZE; x < SCREEN_WIDTH - BORDER_GAP - TILE_SIZE; x+=TILE_SIZE) {
@@ -476,6 +609,7 @@ int main() {
 				SDL_RenderCopy(gRenderer, gTileSheet, &gTileRects[0], &cur_out);
 			}
 		}
+
 
 		//GENERATES TOP BORDER
 		c = BORDER_GAP;
