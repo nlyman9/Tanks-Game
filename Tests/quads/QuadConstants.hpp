3da9a672b74eeca425386295f4b12c9b55470b8b
Evan McEllhenney <ecm53@pitt.edu>
2019-11-18 21:54:19 -0500

    Random generation (#50)
    * centered title screen buttons
    
    * Added horizontal maze map generation
    
    * Enabled map style randomization, added preset map
    
    * updated move/rot speed, restructured map switch
    
    * escape returns to main menu
    
    * new map with holes wip
    
    * fixed line map bug
    
    * checkers preset map created, more of an open level
    
    * Got map generation to work
    
    * added enemy pathfinding
    
    * fixed multiple windows bug on esc
    
    * fixed invisible walls
    
    * added pathway to gameloop
    
    * changed starting locations
    
    * fixed seg fault bug with maze gen
    
    * reverted PHI const value
    
    * fixed collisions (:
    
    * removed unused code
    
    * centered title screen buttons
    
    * merge conflicts
    
    * merge conflicts
    
    * Got map generation to work
    
    * merge conflicts
    
    * reverted PHI const value
    
    * merge conflicts
    
    * Removed duplicated code
    
    * duplicate code
    
    * duplicate code
    
    * copied enemy from master
    
    * replaced array and fixed other room gen bug
    
    * Refactored with classes
    
    * refactored with classes
    
    * added edge class
    
    * added graph.cpp skeleton
    
    * got generation using objects working
    
    * added edges in Quads.cpp
    
    * Edges and graph creation work
    
    * edited graph
    
    * changed graph.hpp and cpp
    
    * FIGURED OUT GRAPH POPULATION YEEEEER
    
    * calculating weights
    
    * testing graph connectivity
    
    * holy mackeral its working
    
    * Map generation finally works
    
    * checkout to master
    
    * Quadrants
    
    * Fixing pull request

--- Tests/quads/QuadConstants.hpp
+++ Tests/quads/QuadConstants.hpp
@@ -1,20 +1,18 @@
-#ifndef _QUAD_CONSTANTS_H_
-#define _QUAD_CONSTANTS_H_
+#ifndef QUADCONSTANTS_HPP
+#define QUADCONSTANTS_HPP
+
+#include <vector>
+
+typedef std::vector<int>       vector_1d;
+typedef std::vector<vector_1d> vector_2d;
+typedef std::vector<vector_2d> vector_3d;
+typedef std::vector<vector_3d> vector_4d;
 
 extern const int MAP_WIDTH;
 extern const int MAP_HEIGHT;
 
 extern const int MAX_ROW;
 extern const int MAX_COL;
-
-// extern const int Q1_MIN_WIDTH, Q4_MIN_WIDTH;
-// extern const int Q1_MAX_WIDTH, Q4_MAX_WIDTH;
-
-// extern const int Q2_MIN_WIDTH, Q5_MIN_WIDTH;
-// extern const int Q2_MAX_WIDTH, Q5_MAX_WIDTH;
-
-// extern const int Q3_MIN_WIDTH, Q6_MIN_WIDTH;
-// extern const int Q3_MAX_WIDTH, Q6_MAX_WIDTH;
 
 extern const int Q1_MIN_HEIGHT, Q2_MIN_HEIGHT, Q3_MIN_HEIGHT;
 extern const int Q1_MAX_HEIGHT, Q2_MAX_HEIGHT, Q3_MAX_HEIGHT;
@@ -31,8 +29,6 @@ extern const int Q3_WIDTH_OFFSET, Q6_WIDTH_OFFSET;
 extern const int Q1_HEIGHT_OFFSET, Q2_HEIGHT_OFFSET, Q3_HEIGHT_OFFSET;
 extern const int Q4_HEIGHT_OFFSET, Q5_HEIGHT_OFFSET, Q6_HEIGHT_OFFSET;
 
-// extern const int MIN_WIDTHS[];
-// extern const int MAX_WIDTHS[];
 extern const int MIN_HEIGHTS[];
 extern const int MAX_HEIGHTS[];
 
