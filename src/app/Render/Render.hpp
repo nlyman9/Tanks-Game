a4a34b76180761d32f9dcb76422c609cbf10211d
nlyman9 <nlyman9@gmail.com>
2019-11-19 13:31:27 -0500

    Revert "removing commits"
    This reverts commit 66070574b9dc4df7bc0df641f8eed4f1dae19d08.

--- src/app/Render/Render.hpp
+++ src/app/Render/Render.hpp
@@ -47,6 +47,7 @@ class Render {
         void setPlayer(std::vector<Player*> players);
         void setPlayerEnemies(std::vector<Player*> players);
         void setEnemies(std::vector<Enemy *> enemies);
+        void setProjectiles(std::vector<Projectile *> projectiles);
     private:
 
         std::vector<SDL_Texture*> gTex;
